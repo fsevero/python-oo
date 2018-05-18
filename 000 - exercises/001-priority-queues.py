aa1de270b076db9b1ca305a8f22338170c70b4a2
Severo <severo.fabricio@gmail.com>
2018-05-17 21:02:15 -0300

    Adds docstrings

--- 000 - exercises/001-priority-queues.py
+++ 000 - exercises/001-priority-queues.py
@@ -1,23 +1,31 @@
 import heapq
 
 class PriorityQueue:
+    '''This class implements a queue with pop priority'''
+
     def __init__(self):
+        '''Inits the queue with initial values'''
         self.queue = []
         self.index = 0
 
     def push(self, item, priority):
+        '''Add a new item to the queue with the informed priority'''
         heapq.heappush(self.queue, (-priority, self.index, item))
         self.index += 1
 
     def pop(self):
+        '''Return the item with the highest priority'''
         return heapq.heappop(self.queue)[-1]
 
 
 class Item:
+    '''Dummy representation of an item'''
     def __init__(self, name):
+        '''Name the item'''
         self.name = name
 
     def __repr__(self):
+        '''Define how to print the dummy data'''
         return self.name
 
 
@@ -29,3 +37,7 @@ q.push(Item('bot'), 10)
 print(q.pop())
 print(q.pop())
 print(q.pop())
+
+print(PriorityQueue.__doc__)
+print(PriorityQueue.push.__doc__)
+help(PriorityQueue)
