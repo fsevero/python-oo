a43e7ec7a80d842ab34f5a6f4b07848669f63c04
Severo <severo.fabricio@gmail.com>
2018-05-28 13:58:13 -0300

    Lazy Singleton

--- 004 - design patterns/singleton.py
+++ 004 - design patterns/singleton.py
@@ -11,9 +11,32 @@ class Singleton(object):
         return cls.instance
 
 s1 = Singleton()
-print(s1)
+# print(s1)
 s2 = Singleton()
-print(s2)
+# print(s2)
 
 assert s1 == s2
 assert s1 is s2
+
+class LazySingleton(object):
+    __instance = None
+
+    def __init__(self):
+        if not LazySingleton.__instance:
+            # print('INIT called')
+            pass
+        else:
+            self.get_instance()
+            # print('Has instance: ', self.get_instance())
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = LazySingleton()
+        return cls.__instance
+
+s1 = LazySingleton()
+LazySingleton.get_instance()
+s2 = LazySingleton()
+
+assert s1.get_instance() is s2.get_instance()
