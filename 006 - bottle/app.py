c6e3e930d429e9fd2c54a3bd05ffcb5a1d919d97
Severo <severo.fabricio@gmail.com>
2018-05-29 10:45:12 -0300

    BOTTLE project - login (get and post)

--- 006 - bottle/app.py
+++ 006 - bottle/app.py
@@ -1,4 +1,4 @@
-from bottle import route, run
+from bottle import route, run, request
 
 @route('/')
 def index():
@@ -9,6 +9,30 @@ def index():
 def hello(name='Anonymous'):
     return '<h1>Hello, {}</h1>'.format(name)
 
+
+@route('/login', method='GET')
+def login():
+    return '''
+    <form action="/login" method="post">
+        username: <input name="username" type="text" />
+        password: <input name="password" type="password" />
+        <input value="Login" type="submit" />
+    </form>
+    '''
+
+@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    if check_login(username, password):
+        return '<h1>Logged as user {}</h1>'.format(username)
+    else:
+        return '<h1>Not allowed</h1>'
+
+
+def check_login(username, password):
+    return False
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
